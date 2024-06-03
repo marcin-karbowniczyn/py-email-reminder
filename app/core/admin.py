61cb428d0675adf2572efe42ac1af56b9a19a89f
Marcin <marcin.karbowniczyn@gmail.com>
2024-06-03 14:02:44 +0200

    Add tests for chanding ad adding users in Django admin, Customize admin page for adding and changing users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin  # noqa
+""" Django admin customization """
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """ Define the admin pages for users """
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Importand dates', {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2')
+        }),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')})
+    )
+    readonly_fields = ['last_login']
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -28,5 +28,23 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
+        self.assertEqual(res.status_code, 200)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test the edit user page works """
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.email)
+
+    def test_add_user_page(self):
+        """ Test the add user page works """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertContains(res, 'Email')
+        self.assertContains(res, 'Password')
+        self.assertEqual(res.status_code, 200)
