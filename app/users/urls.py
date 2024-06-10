35027e7082b8c5bcb88c46118fa04d5f657a7600
Marcin <marcin.karbowniczyn@gmail.com>
2024-06-10 14:25:13 +0200

    Change signup endpoint to create endpoint in urls

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,6 +6,7 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateAuthTokenView.as_view(), name='token')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateAuthTokenView.as_view(), name='token'),
+    path('me/', views.CreateAuthTokenView.as_view(), name='me'),
 ]
