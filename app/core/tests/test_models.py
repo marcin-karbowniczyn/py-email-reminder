d6a0fc287007c36ddb8cd46c81cea4dd593f5999
Marcin <marcin.karbowniczyn@gmail.com>
2024-06-12 11:28:22 +0200

    Add tests for creating Remainder instance

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,10 @@
 """ Tests for models """
+from datetime import date
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+from ..models import Remainder
 
 
 class ModelTests(TestCase):
@@ -27,7 +31,7 @@ class ModelTests(TestCase):
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """ Test creatinfg a user without an email raises a ValueError """
+        """ Test creating a user without an email raises a ValueError """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(email='', password='test1234')
 
@@ -36,3 +40,18 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser(email='superuser@example.com', password='test1234', name='Test User')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_remainder(self):
+        """ Test creating a remainder """
+        user = get_user_model().objects.create_user(email='test@example.com', password='Test1234')
+        current_year = date.today().year
+        remainder = Remainder.objects.create(
+            title='Test Remainder',
+            description='Test Description',
+            remainder_date=date(current_year + 1, 1, 1),
+            permanent='True',
+            user=user
+        )
+
+        self.assertEqual(str(remainder), remainder.title)
+        self.assertEqual(remainder.sent_check, 'None')
---
+++ app/core/validators.py
@@ -0,0 +1,10 @@
+""" Validators for models """
+from datetime import date
+from django.core.exceptions import ValidationError
+
+
+# Remainder validators
+def validate_remainder_date(remainder_date):
+    today = date.today()
+    if remainder_date < date(today.year, today.month, today.day + 1):
+        raise ValidationError('Date cannot be set in the past and needs to be set at least on tomorrow.')
