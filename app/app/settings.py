c3b5c80a8a64722d6dfdf120111b5517fd1ce9a9
Marcin <marcin.karbowniczyn@gmail.com>
2024-09-03 12:41:30 +0200

    Add docker-compose-deploy.yml, add allowed hosts to settings.py, add secret key from env variables, add dbug bool in settings, update docker-compose.yml

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,12 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dvt0(&dfampx*5aq^2u9kuq4^uv@oa_af14t6bi941z2z-7@82'
+SECRET_KEY = os.getenv('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None, os.getenv('ALLOWED_HOSTS', '').split(','))
+)
 
 # Application definition
 
