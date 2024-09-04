533f6fd60d99f074db6f3abf850df06810553203
Marcin <marcin.karbowniczyn@gmail.com>
2024-09-04 16:16:08 +0200

    Add health_check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,8 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from core import views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health_check', core_views.health_check, name='health-check'),
     path('api/users/', include('users.urls')),
     path('api/reminders/', include('reminders.urls'))
 ]
